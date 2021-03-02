9f33664e4ab74ea58905748df2d23d78b1f7081e
Artem Artemev <viiv.c58@gmail.com>
2021-03-02 19:28:04 +0000

    initial working dev env

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = React.useState(0);
+
+  React.useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      console.log(data)
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload!
+          {currentTime}
         </p>
         <a
           className="App-link"
