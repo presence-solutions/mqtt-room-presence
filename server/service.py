869e38f7b542ed8deec685a85340e2adc59c5985
Artem Artemev <viiv.c58@gmail.com>
2021-03-05 19:58:37 +0000

    initalize device trackers from db

--- server/service.py
+++ server/service.py
@@ -1,7 +1,10 @@
+from server.eventbus import eventbus
+from server.events import DeviceAdded
 from server.heartbeat import Heartbeat
 from server.learn import Learn
 from server.predict import Predict
 from server.sensor import Sensor
+from server.models import Session, Device
 
 
 class Service:
@@ -10,3 +13,12 @@ class Service:
         self.learn = Learn()
         self.predict = Predict()
         self.sensor = Sensor()
+
+        self.init_devices()
+
+    def init_devices(self):
+        session = Session()
+        devices = session.query(Device).all()
+
+        for device in devices:
+            eventbus.post(DeviceAdded(device=device))
