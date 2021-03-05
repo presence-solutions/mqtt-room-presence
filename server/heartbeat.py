f1b8e7e6a0bb108e7236031aef67308e9b1d9218
Artem Artemev <viiv.c58@gmail.com>
2021-03-05 19:57:52 +0000

    initial device tracker

--- server/heartbeat.py
+++ server/heartbeat.py
@@ -1,13 +1,39 @@
-from server.events import MQTTConnectedEvent, MQTTMessage
+from server.events import DeviceAdded, DeviceRemoved, MQTTConnectedEvent, MQTTMessage
 from server.eventbus import Mode, eventbus, subscribe
 
 SCANNERS_TOPIC = 'room_presence/'
 
 
+class DeviceTracker:
+    def __init__(self, device):
+        self.device = device
+
+    def kill(self):
+        pass
+
+    def track(self):
+        pass
+
+
 class Heartbeat:
     def __init__(self):
-        print('Init heartbeat', flush=True)
+        self.device_trackers = {}
         eventbus.register(self, self.__class__.__name__)
+
+    @subscribe(on_event=DeviceAdded)
+    def handle_device_added(self, event):
+        self.kill_device(event)
+        if event.device.uuid in self.device_trackers:
+            self.device_trackers[event.device.uuid].kill()
+
+        tracker = DeviceTracker(event.device)
+        self.device_trackers[event.device.uuid] = tracker
+        tracker.track()
+
+    @subscribe(on_event=DeviceRemoved)
+    def handle_device_removed(self, event):
+        if event.device.uuid in self.device_trackers:
+            self.device_trackers[event.device.uuid].kill()
 
     @subscribe(on_event=MQTTConnectedEvent)
     def handle_mqtt_connect(self, event):
@@ -18,5 +44,4 @@ class Heartbeat:
     def handle_mqtt_message(self, event):
         if not event.topic.startswith(SCANNERS_TOPIC):
             return
-
-        print('Message received', event, flush=True)
+        pass
