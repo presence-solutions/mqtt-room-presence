7bee5a60116da415cea6abfe63986dccb79e1310
Vladislav Kosovskikh <kosovskikh.vlad@gmail.com>
2022-02-06 01:05:23 +0000

    added ability to persist state to localstorage

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,12 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
+import throttle from 'lodash/throttle';
 import commonReducer from './slices/commonSlice';
+import { loadState, saveState } from '../lib/utility/localStorage';
+
 import type { Action, ThunkAction } from '@reduxjs/toolkit';
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
     common: commonReducer
-  }
+  },
+  preloadedState
 });
+
+store.subscribe(throttle(() => {
+  const state = store.getState();
+
+  saveState({
+    common: state.common
+  });
+}, 1000));
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
